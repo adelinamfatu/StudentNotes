008c0625fa271756c20247e49b17453aaad55a7d
GabrielaFlorea14 <115966901+GabrielaFlorea14@users.noreply.github.com>
2022-12-28 12:14:58 +0200

    Update CourseDex.js

--- studentnotes/src/components/CourseDex.js
+++ studentnotes/src/components/CourseDex.js
@@ -4,33 +4,31 @@ import { listOfCourses } from "./CoursesList";
 
 class CourseDex extends React.Component {
     render() {
-        return (
-            <div className='CourseDex'>         
+        return (  
+            <div className='CourseDex'> 
+            <div className="cursuri">     
                 <div className='listofCourses'>
                 <h1 id="cursurilemele">Cursurile mele</h1> 
                 <button onclick="adaugaCurs()" id="addCurs">+</button>
                 <button onclick="stergeCurs()" id="removeCurs">-</button>
                 <button onclick="showAlphabetically()" id="namealph">Nume</button>
                 <button onclick="showByDate()" id="date">Data</button>
-
-                <ul id="lista">
-                    {listOfCourses.map((item, index) => {
-                        return(
-                            <div>
-                            <li key={index}>
-                              <a className="courseNme">{item.title}</a>
-                            </li>
-                            </div>
-                        )
-                    })}
-                </ul>
+            
+                <div>
+                    {listOfCourses.filter(course => course.type == "CURS").map(cursListofCourses => (
+                        <a className="courseName">{cursListofCourses.title}</a>
+                    ))}
+                </div> 
 
                 </div>
-            </div>
+            </div>  
+            </div> 
+            
         )
     }
 }
 export default CourseDex
+        
 
 
 //LISTA DE CURSURI
