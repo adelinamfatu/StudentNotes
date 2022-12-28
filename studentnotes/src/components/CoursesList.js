a39ede3236bba578221b1f8a6c38b83dbf0f070e
GabrielaFlorea14 <115966901+GabrielaFlorea14@users.noreply.github.com>
2022-12-28 12:15:28 +0200

    Update CoursesList.js

--- studentnotes/src/components/CoursesList.js
+++ studentnotes/src/components/CoursesList.js
@@ -1,14 +1,20 @@
 export const listOfCourses = [
     //first item
     {
-        title: 'TW'
+        title: 'TW',
+        id: 0, 
+        type: 'SEMINAR'
     }, //index 0
     //second item
     {
-        title: 'CSE'
+        title: 'CSE',
+        id: 1, 
+        type: 'SEMINAR'
     }, //index 1
     //third item
     {
-       title: 'PSI'
+       title: 'PSI',
+       id: 0, 
+       type: 'CURS'
     } //index 2
 ]
