980e70fc35a59b6acf02df9715cbdf83185105e4
GabrielaFlorea14 <115966901+GabrielaFlorea14@users.noreply.github.com>
2022-12-28 12:17:50 +0200

    Update SeminareDex.js

--- studentnotes/src/components/SeminareDex.js
+++ studentnotes/src/components/SeminareDex.js
@@ -1,29 +1,33 @@
 import React from "react";
-import { listOfSeminare } from "./SeminareList";
-import SeminarItem from "./SeminarItem";
+import { listOfCourses } from "./CoursesList";
 
 class SeminareDex extends React.Component {
     render() {
         return (
             <div className='SeminareDex'>
-                <div className='listofSeminare'>
-                <ul>
-                    {listOfSeminare.map((item, index) => {
-                        return(
-                            <div>
-                            <li key={index}>
-                              <a>{item.title}</a>
-                            </li>
-                            </div>
-                        )
-                    })}
-                </ul>
+            <div className="cursuri">     
+                <div className='listofCourses'>
+                <h1 id="cursurilemele">Seminarele mele</h1> 
+                <button onclick="adaugaCurs()" id="addCurs">+</button>
+                <button onclick="stergeCurs()" id="removeCurs">-</button>
+                <button onclick="showAlphabetically()" id="namealph">Nume</button>
+                <button onclick="showByDate()" id="date">Data</button>
+            
+                <div>
+                    {listOfCourses.filter(course => course.type == "SEMINAR").map(seminarListofCourses => (
+                        <a className="courseName">{seminarListofCourses.title}</a>
+                    ))}
+                </div> 
+
                 </div>
-            </div>
+            </div>  
+            </div> 
         )
     }
 }
 export default SeminareDex
+
+
 
 
 //LISTA DE SEMINARE
