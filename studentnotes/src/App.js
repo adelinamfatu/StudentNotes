0b42d88b8d680f394425cc648dccde8f5d9af866
GabrielaFlorea14 <115966901+GabrielaFlorea14@users.noreply.github.com>
2022-12-30 12:36:04 +0200

    Update App.js

--- studentnotes/src/App.js
+++ studentnotes/src/App.js
@@ -1,33 +1,37 @@
-import React, { useState} from 'react';
-
-import './App.css';
-import './style/Login-Register.css';
+import logo from './logo.svg';
+import Profile from './components/Profile';
+import Groups from './components/Groups';
+import Cursuri from './components/Cursuri';
+import Seminare from './components/Seminare';
 import Login from './components/Login';
 import Register from './components/Register';
-
+import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+  Routes,
+  Route
+ } from "react-router-dom";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
-  <div className="App">
-    {
-      currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>       
-    }
+    <div className="App">
+ 
+      <Router>
+      <Routes>
+      <Route path="/" element={< Login/>} />
+      <Route path="/register" element={< Register/>} />
+      <Route path="/cursuri" element={< Cursuri/>} />
+        <Route path="/seminare" element={<Seminare />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/grupuri" element={<Groups />} />
 
-  </div>
+
+      </Routes>
+    </Router>
+    
+    </div>
   );
 }
+
 
 export default App;
